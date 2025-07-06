eebc74b959c3222e4c878059e71a2ed47b522cad
yonghwadragon <yongyonghwa@naver.com>
2025-07-06 17:55:36 +0900

    chart

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <ThreeScene />
+  <SpeedChart />
 </template>
 
 <script setup>
 import ThreeScene from './components/ThreeScene.vue'
+import SpeedChart from './components/SpeedChart.vue'
 </script>
---
+++ src/components/SpeedChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <canvas ref="chartCanvas"></canvas>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { Chart } from 'chart.js/auto'
+
+const chartCanvas = ref(null)
+
+onMounted(() => {
+  new Chart(chartCanvas.value, {
+    type: 'line',
+    data: {
+      labels: ['12:00', '12:01', '12:02'],
+      datasets: [{
+        label: 'Speed (km/h)',
+        data: [35, 38, 42],
+        borderColor: 'red',
+        tension: 0.3
+      }]
+    }
+  })
+})
+</script>
